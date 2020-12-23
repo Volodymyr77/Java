4fd390501cd86c8243a307e0ccddb0e807581a01
Volodymyr77 <gagarinvov@gmail.com>
2020-12-24 00:21:39 +0200

    Thread

--- src/lesson9/Main.java
+++ src/lesson9/Main.java
@@ -2,37 +2,40 @@ package lesson9;
 
 public class Main {
 
-    private static boolean flag = true;
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread("Full-thread-1", "Roman");
+        myThread.start();
 
-//    public static void main(String[] args) {
-//        MyThread myThread = new MyThread("Full-Thread-1", "Roman");
-//        myThread start();
-//
-//        Runnable runnable = () => {
-//            for (int i= 0; i<5; i++) {
+//        Runnable runnable = () -> {
+//            for (int i = 0; i < 5; i++) {
 //                System.out.println(Thread.currentThread().getName() + ":" + i);
 //            }
+//        };
+//        Thread thread = new Thread(new MyThread(), "My-Tread-1");
+//        thread.start();
+    }
+
 //            for (int i= 0; i<5; i++) {
 //                System.out.println(Thread.currentThread().getName() + ":" + i);
 //            }
 //        }
 //    }
 
-    Thread thread1 = new Thread() -> {
-        for (int i=0; i<2000; i++) {
-            System.out.println("value" + i);
-        }
-        flag = false;
-        System.out.println("flag status changed to " + flag);
-    }
-    Thread thread2 = new Thread(new Runnable() {
-        int i=1;
-        @Override
-        public void run() {
-            while (flag) {
-                i++;
-            }
-            System.out.println("another flag");
-        }
-    })
+//    Thread thread1 = new Thread() > {
+//        for (int i=0; i<2000; i++) {
+//            System.out.println("value" + i);
+//        }
+//        flag = false;
+//        System.out.println("flag status changed to " + flag);
+//    }
+//    Thread thread2 = new Thread(new Runnable() {
+//        int i=1;
+//        @Override
+//        public void run() {
+//            while (flag) {
+//                i++;
+//            }
+//            System.out.println("another flag");
+//        }
+//    })
 }
--- src/lesson9/MyThread.java
+++ src/lesson9/MyThread.java
@@ -1,7 +1,19 @@
 package lesson9;
 
 public class MyThread extends Thread {
+
     private String whoCreated;
 
-    public MyThread
+    public MyThread(String name, String whoCreated) {
+        super(name);
+        this.whoCreated = whoCreated;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(whoCreated);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(this.getName() + ":" + i);
+        }
+    }
 }
